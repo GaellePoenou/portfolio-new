7c12fe29133cd978afecf37e124e64d0dcba2bd4
GaellePoenou <galle.poenou2023@campus-eni.fr>
2024-01-01 19:03:35 +0100

    css card

--- Components/ProjectCard.js
+++ Components/ProjectCard.js
@@ -12,22 +12,32 @@ const ProjectCard = ({ project }) => {
       <img
         src={project.image}
         alt={project.title}
-        className="cursor-pointer rounded-md"
+        className="cursor-pointer rounded-md hover:shadow-lg transition-all duration-300"
         onClick={toggleDetails}
       />
 
       {showDetails && (
         <div className="absolute top-0 left-0 right-0 bottom-0 bg-white p-4 rounded-md shadow-md">
-          <h3 className="text-xl font-bold mb-2">{project.title}</h3>
-          <p>{project.description}</p>
+          <h3 className="text-xl font-bold mb-2 text-[#496a7a]">
+            {project.title}
+          </h3>
+          <p className="text-gray-700 mb-4">{project.description}</p>
           <a
             href={project.githubLink}
             target="_blank"
             rel="noopener noreferrer"
+            className="text-[#496a7a] hover:underline"
           >
             Voir sur GitHub
           </a>
-          <button onClick={toggleDetails}>Fermer</button>
+          <div className="">
+            <button
+              onClick={toggleDetails}
+              className="bg-[#496a7a] text-white px-4 py-2 rounded-md mt-4 hover:bg-[#8aa3a7] transition-all duration-200"
+            >
+              Fermer
+            </button>
+          </div>
         </div>
       )}
     </div>
