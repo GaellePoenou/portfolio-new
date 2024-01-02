ca6a550505273ef66c01543f27bb3f45f266661f
GaellePoenou <galle.poenou2023@campus-eni.fr>
2024-01-02 16:21:16 +0100

    git

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/portfolio-new",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  outDir: "out",
+
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
 };
 
 module.exports = nextConfig;
