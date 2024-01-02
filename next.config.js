93ec379c2069a9b34b6322420675e7bae5732e72
GaellePoenou <galle.poenou2023@campus-eni.fr>
2024-01-02 17:23:31 +0100

    Correction de l'erreur d'optimisation d'image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 
   exportPathMap: async function () {
     return {
