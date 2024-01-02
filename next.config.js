a829ee84c9bc5b50e118a80742982179e1a47358
GaellePoenou <galle.poenou2023@campus-eni.fr>
2024-01-02 14:47:38 +0100

    Configuration du déploiement sur GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/portfolio-new",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
